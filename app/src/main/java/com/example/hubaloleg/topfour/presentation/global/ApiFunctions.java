acb21de280fe3a41d0e7a6bf1d78533c1ad06e09
Konstantin <skifoner@mail.ru>
2017-07-10 14:13:34 +0300

    Added endpoints

---
+++ app/src/main/java/com/example/hubaloleg/topfour/presentation/global/ApiFunctions.java
@@ -0,0 +1,10 @@
+package com.example.hubaloleg.topfour.presentation.global;
+
+/**
+ * Created by kostya on 10.07.17.
+ */
+
+public class ApiFunctions {
+    public static final String GET_PROFILE_INFO = "users/self";
+    public static final String GET_VENUE_HISTORY = "users/self/venuehistory";
+}
