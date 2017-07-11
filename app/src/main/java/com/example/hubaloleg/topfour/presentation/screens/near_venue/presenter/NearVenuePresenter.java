7a7342232cfefbee3fec253713dac6177e1ca7fd
hubaloleg <hubal.oleg@gmail.com>
2017-07-11 12:03:03 +0300

    Add near venue fragment to Main Activity

---
+++ app/src/main/java/com/example/hubaloleg/topfour/presentation/screens/near_venue/presenter/NearVenuePresenter.java
@@ -0,0 +1,10 @@
+package com.example.hubaloleg.topfour.presentation.screens.near_venue.presenter;
+
+import com.arellomobile.mvp.InjectViewState;
+import com.arellomobile.mvp.MvpPresenter;
+import com.example.hubaloleg.topfour.presentation.screens.near_venue.view.NearVenueView;
+
+@InjectViewState
+public class NearVenuePresenter extends MvpPresenter<NearVenueView> {
+
+}
