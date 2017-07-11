b47249d796f476a55b7ebfff30a71b942a496f53
hubaloleg <hubal.oleg@gmail.com>
2017-07-11 11:35:10 +0300

    Change NearVenue to Main Activity

---
+++ app/src/main/java/com/example/hubaloleg/topfour/presentation/screens/main/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.hubaloleg.topfour.presentation.screens.main;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.hubaloleg.topfour.R;
+
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    public static Intent getIntent(Context context) {
+        return new Intent(context, MainActivity.class);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(MainActivity.this);
+    }
+}
--- app/src/main/java/com/example/hubaloleg/topfour/presentation/screens/splash/presenter/SplashPresenter.java
+++ app/src/main/java/com/example/hubaloleg/topfour/presentation/screens/splash/presenter/SplashPresenter.java
@@ -1,10 +1,7 @@
 package com.example.hubaloleg.topfour.presentation.screens.splash.presenter;
 
-import android.util.Log;
-
 import com.arellomobile.mvp.InjectViewState;
 import com.arellomobile.mvp.MvpPresenter;
-import com.example.hubaloleg.topfour.data.local.prefs.CredentialStore;
 import com.example.hubaloleg.topfour.domain.usecase.CredentialUseCase;
 import com.example.hubaloleg.topfour.presentation.screens.splash.view.SplashView;
 
@@ -13,7 +10,6 @@ import javax.inject.Inject;
 @InjectViewState
 public class SplashPresenter extends MvpPresenter<SplashView> {
 
-    private static final String TAG = "SplashPresenter";
     private final CredentialUseCase mCredentialUseCase;
 
     @Inject
